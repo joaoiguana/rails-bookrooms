2a3e8fd208e9aa9c6d5e1a128e1cea3460f8eaf2
joao garcia <jiboiadamor@gmail.com>
2023-02-08 12:05:50 +0000

    tried to add metric cards to avo

---
+++ app/avo/dashboards/dash.rb
@@ -0,0 +1,12 @@
+class Dash < Avo::Dashboards::BaseDashboard
+  self.id = "dash"
+  self.name = "Dash"
+  # self.description = "Tiny dashboard description"
+  # self.grid_cols = 3
+  # self.visible = -> do
+  #   true
+  # end
+
+  # # cards go here
+  # card BookingsCount
+end
